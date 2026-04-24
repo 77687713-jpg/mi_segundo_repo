92d8a699541ed2ea0ca4374c7be7c3b455113862
77687713-jpg <77687713@institutourusayhua.edu.pe>
2026-04-24 09:47:46 -0500

    nueva funcionalidad en rama3

--- app.py
+++ app.py
@@ -1,15 +1,1 @@
-<<<<<<< HEAD
-#mi primer programa
-<<<<<<< HEAD
-<<<<<<< HEAD
-print("main-sistema base")
-=======
-print("hola mundo")
-print("RAMA 1 - Registro de usuarios")
->>>>>>> rama1
-=======
-print("RAMA 2 - Login de usuarios")
->>>>>>> rama2
-=======
-print("RAMA 3 - Panel administrativo")
->>>>>>> rama3
+print("RAMA 3 - Nueva funcionalidad")
